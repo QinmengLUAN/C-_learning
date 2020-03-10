6064443fab72fbe405b74b5c7960a0312384b33a
Qinmeng Luan <31704318+QinmengLUAN@users.noreply.github.com>
2020-03-10 21:58:00 +0000

    Part 1_variables, calculator, if , while

---
+++ bartender.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(){
+  int age;
+  cout << "Welcome to the pub!" << endl;
+  cout << "What is your age?" << endl;
+  cin >> age;
+  if(age > 18){
+    cout << "Have a pint!" << endl;
+  } else if(age ==18){
+    cout << "Show me your ID and then have a pint!" << endl;
+  } else{
+    cout << "I'm calling the police." << endl;
+  }
+  cout << "Goodbye" << endl;
+}
