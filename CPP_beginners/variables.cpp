ce905a656f78492522a7dbfb6aef7879ce1d7196
QinmengLUAN <qinmengluan@gmail.com>
2020-12-20 00:07:47 +0000

    modify

---
+++ CPP_beginners/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double y;
+    x = -3.9;
+    y = 3.142;
+    cout << "The value of x is " << x << " and the value of y is " << y << endl;
+    return 0;
+}
