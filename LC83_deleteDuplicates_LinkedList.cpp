c4b45d7363ad12109a59b4f1aa48646c7f322e40
QinmengLUAN <qinmengluan@gmail.com>
2021-01-13 23:24:51 +0000

    Leetcode83 Linked List

---
+++ LC83_deleteDuplicates_LinkedList.cpp
@@ -0,0 +1,40 @@
+/*
+83. Remove Duplicates from Sorted List
+Easy
+
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* node = head;
+        while (node != nullptr && node->next != nullptr) {
+            if (node->val == node->next->val) {
+                node->next = node->next->next;
+            } else {
+                node = node->next;
+            }
+        }
+        return head;
+    }
+};
